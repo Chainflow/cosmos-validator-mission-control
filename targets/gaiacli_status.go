f7a742b7975ecab48cac14a6adffdc86f5606fe6
Akshay Gupta <akshaydotsh@gmail.com>
2020-02-20 01:22:57 +0530

    * Metrics from gaiacli status added with influxDBA
    Signed-off-by: Akshay Gupta <akshaydotsh@gmail.com>

--- targets/gaiacli_status.go
+++ targets/gaiacli_status.go
@@ -11,6 +11,12 @@ import (
 )
 
 func GetGaiaCliStatus(_ HTTPOptions, cfg *config.Config, c client.Client) {
+	bp, err := createBatchPoints(cfg.InfluxDB.Database)
+	if err != nil {
+		return
+	}
+	var pts []*client.Point
+
 	cmd := exec.Command("gaiacli", "status")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
@@ -30,24 +36,44 @@ func GetGaiaCliStatus(_ HTTPOptions, cfg *config.Config, c client.Client) {
 		_ = SendTelegramAlert("Validator has been jailed!", cfg)
 		_ = SendEmailAlert("Validator has been jailed!", cfg)
 	}
+	p1, err := createDataPoint("vcf_validator_status", map[string]string{}, map[string]interface{}{"status": validatorActive})
+	if err == nil {
+		pts = append(pts, p1)
+	}
 
 	currentBlockHeight := status.NodeInfo.SyncInfo.LatestBlockHeight
+	if currentBlockHeight != "" {
+		p2, err := createDataPoint("vcf_current_block_height", map[string]string{}, map[string]interface{}{"height": currentBlockHeight})
+		if err == nil {
+			pts = append(pts, p2)
+		}
+	}
 
 	caughtUp := !status.NodeInfo.SyncInfo.CatchingUp
 	if caughtUp {
 		_ = SendTelegramAlert("Your node has been synced!", cfg)
 		_ = SendEmailAlert("Your node has been synced!", cfg)
 	}
+	p3, err := createDataPoint("vcf_node_synced", map[string]string{}, map[string]interface{}{"status": caughtUp})
+	if err == nil {
+		pts = append(pts, p3)
+	}
 
 	vp, err := strconv.Atoi(status.NodeInfo.ValidatorInfo.VotingPower)
 	if err != nil {
 		log.Printf("Error while converting votingPower to int: %v", err)
+	}
+	p4, err := createDataPoint("vcf_voting_power", map[string]string{}, map[string]interface{}{"power": vp})
+	if err == nil {
+		pts = append(pts, p4)
 	}
 	if int64(vp) <= cfg.VotingPowerThreshold {
 		_ = SendTelegramAlert(fmt.Sprintf("Your voting power has dropped below %d", cfg.VotingPowerThreshold), cfg)
 		_ = SendEmailAlert(fmt.Sprintf("Your voting power has dropped below %d", cfg.VotingPowerThreshold), cfg)
 	}
 
+	bp.AddPoints(pts)
+	_ = writeBatchPoints(c, bp)
 	log.Printf("Validator Active: %t \nCurrent Block Height: %s \nCaught Up? %t \nVoting Power: %d \n",
 		validatorActive, currentBlockHeight, caughtUp, vp)
 }
