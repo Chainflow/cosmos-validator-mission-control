00a220b0d499e0d9a03912f80ed9ec80ba810678
Akshay Gupta <akshaydotsh@gmail.com>
2020-02-20 12:29:38 +0530

    * Metrics for operator info in influxDB done  - operator addr  - addr  - fee  - desc  - max rate  - max change rate
    Signed-off-by: Akshay Gupta <akshaydotsh@gmail.com>

--- targets/operator_info.go
+++ targets/operator_info.go
@@ -9,6 +9,12 @@ import (
 )
 
 func GetOperatorInfo(ops HTTPOptions, cfg *config.Config, c client.Client) {
+	bp, err := createBatchPoints(cfg.InfluxDB.Database)
+	if err != nil {
+		return
+	}
+	var pts []*client.Point
+
 	resp, err := HitHTTPTarget(ops)
 	if err != nil {
 		log.Printf("Error: %v", err)
@@ -23,12 +29,28 @@ func GetOperatorInfo(ops HTTPOptions, cfg *config.Config, c client.Client) {
 	}
 
 	operatorAddress := validatorResp.Validator.Details.OperatorAddress
+	p1, err := createDataPoint("vcf_operator_address", map[string]string{}, map[string]interface{}{"address": operatorAddress})
+	if err == nil {
+		pts = append(pts, p1)
+	}
 
 	address := validatorResp.Validator.Uptime.Address
+	p2, err := createDataPoint("vcf_address", map[string]string{}, map[string]interface{}{"address": address})
+	if err == nil {
+		pts = append(pts, p2)
+	}
 
 	fee := validatorResp.Validator.Details.Commission.Rate
+	p3, err := createDataPoint("vcf_validator_fee", map[string]string{}, map[string]interface{}{"rate": fee})
+	if err == nil {
+		pts = append(pts, p3)
+	}
 
 	validatorDetails := validatorResp.Validator.Details.Description
+	p4, err := createDataPoint("vcf_validator_desc", map[string]string{}, map[string]interface{}{"desc": validatorDetails})
+	if err == nil {
+		pts = append(pts, p4)
+	}
 
 	var maxRate float64
 	mr, err := strconv.ParseFloat(validatorResp.Validator.Details.Commission.MaxRate, 64)
@@ -36,6 +58,10 @@ func GetOperatorInfo(ops HTTPOptions, cfg *config.Config, c client.Client) {
 		maxRate = 0
 	} else {
 		maxRate = mr * 100
+	}
+	p5, err := createDataPoint("vcf_validator_max_rate", map[string]string{}, map[string]interface{}{"rate": maxRate})
+	if err == nil {
+		pts = append(pts, p5)
 	}
 
 	var maxChangeRate float64
@@ -46,7 +72,13 @@ func GetOperatorInfo(ops HTTPOptions, cfg *config.Config, c client.Client) {
 	} else {
 		maxChangeRate = mcr * 100
 	}
+	p6, err := createDataPoint("vcf_validator_max_change_rate", map[string]string{}, map[string]interface{}{"rate": maxChangeRate})
+	if err == nil {
+		pts = append(pts, p6)
+	}
 
+	bp.AddPoints(pts)
+	_ = writeBatchPoints(c, bp)
 	log.Printf("Ooperator Addr: %s \nAddress: %s \nFee: %s \nValidator Details: %v \nMax Rate: %f \nMax Change Rate: %f \n",
 		operatorAddress, address, fee, validatorDetails, maxRate, maxChangeRate)
 }
