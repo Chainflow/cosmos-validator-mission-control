ee68622b2b17a92d9f745f29ed531563dd506a26
anilCSE <anil@vitwit.com>
2020-02-28 11:56:53 +0530

    Update godoc

--- targets/alert.go
+++ targets/alert.go
@@ -6,6 +6,7 @@ import (
 	"log"
 )
 
+// SendTelegramAlert sends the alert to telegram chat
 func SendTelegramAlert(msg string, cfg *config.Config) error {
 	if err := alerting.NewTelegramAlerter().Send(msg, cfg.Telegram.BotToken, cfg.Telegram.ChatId); err != nil {
 		log.Printf("failed to send tg alert: %v", err)
