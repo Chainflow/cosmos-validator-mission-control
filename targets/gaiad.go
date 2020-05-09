49bb030e39ca50ca8e390dace8710bcfe227d9c6
PrathyushaLakkireddy <prathyusha@vitwit.com>
2020-05-09 22:17:10 +0530

    Revert back the changes to get gaiad version

--- targets/gaiad.go
+++ targets/gaiad.go
@@ -2,34 +2,40 @@ package targets
 
 import (
 	"chainflow-vitwit/config"
-	"encoding/json"
 	"log"
+	"os/exec"
+	"regexp"
 
 	client "github.com/influxdata/influxdb1-client/v2"
 )
 
 // GaiadVersion to get gaiad version by running command gaiad version
-func GaiadVersion(ops HTTPOptions, cfg *config.Config, c client.Client) {
+func GaiadVersion(_ HTTPOptions, cfg *config.Config, c client.Client) {
 	bp, err := createBatchPoints(cfg.InfluxDB.Database)
 	if err != nil {
 		return
 	}
 
-	resp, err := HitHTTPTarget(ops)
+	cmd := exec.Command("gaiad", "version", "--long")
+	out, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Printf("Error: %v", err)
+		log.Printf("cmd.Run() failed with %s\n", err)
+		_ = writeToInfluxDb(c, bp, "vcf_gaiad_version", map[string]string{}, map[string]interface{}{"v": "NA"})
 		return
 	}
 
-	var statusInfo GaiadStatusInfo
-	err = json.Unmarshal(resp.Body, &statusInfo)
-	if err != nil {
-		log.Printf("Error: %v", err)
+	resp := string(out)
+
+	r := regexp.MustCompile(`version: ([0-9]{1}.[0-9]{1}.[0-9]{1})`)
+	matches := r.FindAllStringSubmatch(resp, -1)
+	if len(matches) == 0 {
+		_ = writeToInfluxDb(c, bp, "vcf_gaiad_version", map[string]string{}, map[string]interface{}{"v": "NA"})
 		return
 	}
-
-	version := statusInfo.Result.NodeInfo.Version
-
-	_ = writeToInfluxDb(c, bp, "vcf_gaiad_version", map[string]string{}, map[string]interface{}{"v": version})
-	log.Printf("Version: %s", version)
+	if len(matches[0]) != 2 {
+		_ = writeToInfluxDb(c, bp, "vcf_gaiad_version", map[string]string{}, map[string]interface{}{"v": "NA"})
+		return
+	}
+	_ = writeToInfluxDb(c, bp, "vcf_gaiad_version", map[string]string{}, map[string]interface{}{"v": matches[0][1]})
+	log.Printf("Version: %s", matches[0][1])
 }
