f0bb67583fc9ce71325adf1f4fee6629569c7c8d
PrathyushaLakkireddy <prathyusha@vitwit.com>
2020-05-21 15:18:37 +0530

    Updated shell script with systemd services

--- install_script.sh
+++ install_script.sh
@@ -8,21 +8,32 @@ export TELEGRAF=telegraf
 teleFalg="$1"
 teleFlagValue="--remote-hosted"
 
-wget https://dl.grafana.com/oss/release/grafana-6.7.2.linux-amd64.tar.gz
+echo "----------- Installing grafana -----------"
 
-tar -zxvf grafana-6.7.2.linux-amd64.tar.gz
+sudo apt-get install -y adduser libfontconfig1
 
-cd grafana-6.7.2/bin/
+wget https://dl.grafana.com/oss/release/grafana_6.7.2_amd64.deb
 
-./grafana-server &
+sudo dpkg -i grafana_6.7.2_amd64.deb
+
+echo "------ Starting grafana server using systemd --------"
+
+sudo systemctl daemon-reload
+
+sudo systemctl start grafana-server
+
+sudo systemctl status grafana-server
 
 cd $HOME
+
+echo "----------- Installing Influx -----------"
 
 wget https://dl.influxdata.com/influxdb/releases/influxdb-1.7.10_linux_amd64.tar.gz
 
 tar xvfz influxdb-1.7.10_linux_amd64.tar.gz
 
 echo "-----------Fetching influxd path--------"
+
 INFLUXD_PATH=$(which $INFLUX_DAEMON)
 
 echo "---------Creating influxd system file---------"
@@ -40,6 +51,8 @@ LimitNOFILE=4096
 WantedBy=multi-user.target
 " >$INFLUX_DAEMON.service
 
+echo "------- Running influxd by using systemd ------"
+
 sudo mv $INFLUX_DAEMON.service /lib/systemd/system/$INFLUX_DAEMON.service
 sudo -S systemctl daemon-reload
 sudo -S systemctl start $INFLUX_DAEMON
@@ -48,11 +61,13 @@ cd $HOME
 
 if [ "$teleFalg" != "$teleFlagValue" ];
 then 
+	echo "----------- Installing telegraf -----------------"
 	wget https://dl.influxdata.com/telegraf/releases/telegraf-1.14.0_linux_amd64.tar.gz
 
 	tar xf telegraf-1.14.0_linux_amd64.tar.gz
 	
 	echo "-----------Fetching telegraf path--------"
+
 	TELEGRAF_PATH=telegraf/usr/bin/telegraf
 
 	echo "---------Creating telegraf system file---------"
@@ -70,6 +85,8 @@ then
 	WantedBy=multi-user.target
 	" >$TELEGRAF.service
 
+	echo "---------- Running telegraf by using systemd ---------"
+
 	sudo mv $TELEGRAF.service /lib/systemd/system/$TELEGRAF.service
 	sudo -S systemctl daemon-reload
 	sudo -S systemctl start $TELEGRAF
@@ -78,17 +95,23 @@ else
 	echo "--remote-hosted enabled, so not downloading the telegraf"
 fi
 
+echo "--------- Cloning cosmos-validator-mission-control -----------"
+
 git clone git@github.com:Chainflow/cosmos-validator-mission-control.git
 
 cd cosmos-validator-mission-control
 
 cp example.config.toml config.toml
 
+echo "-------- Creating database in influx ----------"
+
 influx
 
 CREATE DATABASE vcf
 
 exit
+
+echo "------ Building and running the code --------"
 
 go build && ./cosmos-validator-mission-control &
 
