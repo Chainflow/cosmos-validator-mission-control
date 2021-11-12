963d9b3f8baf5324576e070e3fa3e308b0acd3ec
Prathyusha Lakkireddy <prathyushareddy.lakkireddy@gmail.com>
2021-11-12 11:28:01 +0530

    update format

--- alert-bot/server/rpc_status.go
+++ alert-bot/server/rpc_status.go
@@ -18,7 +18,6 @@ func GetEndpointStatus(cfg *config.Config) error {
 	if err != nil {
 		log.Printf("Error: %v", err)
 		msg = msg + fmt.Sprintf("⛔ Unreachable to %s RPC :: and the ERROR is: %v", cfg.ExternalRPC, err)
-
 	}
 
 	if msg != "" {
